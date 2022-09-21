a8924adb23fe6ccb000812499e3fbc48ebcfc098
Md. Rahadul Islam Fardin <85452696+mrif449@users.noreply.github.com>
2022-09-21 15:03:36 +0600

    Add files via upload

---
+++ Digital Clock/main.py
@@ -0,0 +1,14 @@
+from time import strftime
+from tkinter import *
+from tkinter.ttk import *
+
+display= Tk()
+display.title("Digital Clock")
+def digitalClock():
+	text = strftime(" %H:%M:%S %p ")
+	label.config(text = text)
+	label.after(1000,digitalClock)
+label = Label(display, font= ("Impact regular", 100, "bold"), background="black",foreground="#00FF7F")
+label.pack(anchor="center")
+digitalClock()
+mainloop()
