c628a95c21956ab0ee5fe3d78bff8f963366b132
Md. Rahadul Islam Fardin <85452696+mrif449@users.noreply.github.com>
2022-11-19 15:06:13 +0600

    Update main.py

--- Digital Clock/main.py
+++ Digital Clock/main.py
@@ -3,12 +3,13 @@ from tkinter import *
 from tkinter.ttk import *
 
 display= Tk()
+display.geometry("1100x200")
 display.title("Digital Clock")
 def digitalClock():
 	text = strftime(" %H:%M:%S %p ")
 	label.config(text = text)
 	label.after(1000,digitalClock)
-label = Label(display, font= ("Digital Dream Regular", 100, "bold"), background="black",foreground="#00FF7F")
+label = Label(display, font= ("high speed", 100, "bold"), background="black",foreground="#00FF7F")
 label.pack(anchor="center")
 digitalClock()
 mainloop()
